ea58c05ecb038f799b9b30087d805b0888fb8c33
Viacheslav Butorov <butorovv@gmail.com>
2017-01-18 01:40:56 +0700

    Added register.

--- sentry_telegram/__init__.py
+++ sentry_telegram/__init__.py
@@ -4,6 +4,11 @@ Plugin for Sentry which allows sending notification via Telegram messenger.
 
 DISCLAIMER: Tested only with Sentry 8.9.0
 """
+from __future__ import absolute_import
+from sentry.plugins import register
+from .plugin import TelegramNotificationsPlugin
 
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
+
+register(TelegramNotificationsPlugin)
