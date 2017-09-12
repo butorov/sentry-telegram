3d05bed6f102d22aeb4029f6141a88c54f38dc1d
Viacheslav Butorov <butorovv@gmail.com>
2017-09-13 01:33:47 +0700

    Better test's design.

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,7 @@
 # coding: utf-8
-from mock import call, patch
+from mock import patch
+from distutils.version import LooseVersion as V
+
 import pytest
 
 import sentry
@@ -9,6 +11,9 @@ from sentry.utils.samples import create_sample_event
 
 
 from sentry_telegram import TelegramNotificationsPlugin
+
+
+sentry_version = V(sentry.__version__)
 
 
 class BaseTest(PluginTestCase):
@@ -21,8 +26,7 @@ class BaseTest(PluginTestCase):
     def test_is_registered(self):
         assert plugins.get('sentry_telegram').slug == self.plugin.slug
 
-    @pytest.mark.skipif(not sentry.__version__.startswith('8.9.0'), reason='sentry versions newer than 8.9.0 message text is longer than title')
-    def test_old_complex_send_notification(self):
+    def send_notification_helper(self):
         self.initialized_plugin.set_option('receivers', '123', self.project)
         self.initialized_plugin.set_option('api_token', 'api:token', self.project)
         self.initialized_plugin.set_option(
@@ -34,49 +38,49 @@ class BaseTest(PluginTestCase):
         notification = Notification(event=event)
         with patch('requests.sessions.Session.request') as request:
             self.initialized_plugin.notify(notification)
-            assert request.call_args_list == [
-                call(
-                    allow_redirects=False,
-                    method='POST',
-                    headers={'Content-Type': 'application/json'},
-                    url=u'https://api.telegram.org/botapi:token/sendMessage',
-                    json={
-                        'text': u'*[Sentry]* Bar error: This is an example python exception\nThis is an example python exception\nhttp://testserver/baz/bar/issues/1/',
-                        'parse_mode': 'Markdown',
-                        'chat_id': '123',
-                    },
-                    timeout=30,
-                    verify=True,
-                ),
-            ]
+            return request
 
-    @pytest.mark.skipif(sentry.__version__.startswith('8.9.0'), reason='sentry 8.9.0 message text equals to title')
-    def test_complex_send_notification(self):
-        self.initialized_plugin.set_option('receivers', '123', self.project)
-        self.initialized_plugin.set_option('api_token', 'api:token', self.project)
-        self.initialized_plugin.set_option(
-            'message_template',
-            '*[Sentry]* {project_name} {tag[level]}: {title}\n{message}\n{url}',
-            self.project,
+    @staticmethod
+    def assert_notification_helper(request_call, message_text):
+        assert request_call == dict(
+            allow_redirects=False,
+            method='POST',
+            headers={'Content-Type': 'application/json'},
+            url='https://api.telegram.org/botapi:token/sendMessage',
+            json={
+                'text': message_text,
+                'parse_mode': 'Markdown',
+                'chat_id': '123',
+            },
+            timeout=30,
+            verify=True,
         )
-        event = create_sample_event(self.project, platform='python')
-        notification = Notification(event=event)
-        with patch('requests.sessions.Session.request') as request:
-            self.initialized_plugin.notify(notification)
-            assert request.call_count == 1
-            assert request.call_args_list[0][1] == dict(
-                allow_redirects=False,
-                method='POST',
-                headers={'Content-Type': 'application/json'},
-                url='https://api.telegram.org/botapi:token/sendMessage',
-                json={
-                    'text': '*[Sentry]* Bar error: This is an example Python exception\nThis is an example Python exception raven.scripts.runner in main\nhttp://testserver/baz/bar/issues/1/',
-                    'parse_mode': 'Markdown',
-                    'chat_id': '123',
-                },
-                timeout=30,
-                verify=True,
-            )
+
+    @pytest.mark.skipif(sentry_version > V('8.9.0'), reason='sentry versions newer than 8.9.0 message text is longer than title')
+    def test_old_complex_send_notification(self):
+        request = self.send_notification_helper()
+        self.assert_notification_helper(
+            request.call_args_list[0][1],
+            '*[Sentry]* Bar error: This is an example python exception\nThis is an example python exception\nhttp://testserver/baz/bar/issues/1/',
+        )
+
+    @pytest.mark.skipif(V('8.19.0') < sentry_version or sentry_version <= V('8.9.0'), reason='sentry 8.9.0 message text equals to title')
+    def test_complex_send_notification(self):
+        request = self.send_notification_helper()
+        assert request.call_count == 1
+        self.assert_notification_helper(
+            request.call_args_list[0][1],
+            '*[Sentry]* Bar error: This is an example python exception\nThis is an example python exception raven.scripts.runner in main\nhttp://testserver/baz/bar/issues/1/',
+        )
+
+    @pytest.mark.skipif(sentry_version < V('8.20.0'), reason='sentry 8.9.0 message text equals to title')
+    def test_complex_send_notification(self):
+        request = self.send_notification_helper()
+        assert request.call_count == 1
+        self.assert_notification_helper(
+            request.call_args_list[0][1],
+            '*[Sentry]* Bar error: This is an example Python exception\nThis is an example Python exception raven.scripts.runner in main\nhttp://testserver/baz/bar/issues/1/',
+        )
 
     def test_get_empty_receivers_list(self):
         self.initialized_plugin.set_option('receivers', '', self.project)
