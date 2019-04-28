873475de410bc256d7f113d694ec3a57228c3762
Viacheslav Butorov <butorovv@gmail.com>
2019-04-28 19:50:56 +0200

    Try to improve installation process. Drop not necessary part.

--- sentry_telegram/__init__.py
+++ sentry_telegram/__init__.py
@@ -2,19 +2,6 @@
 """
 Plugin for Sentry which allows sending notification via Telegram messenger.
 """
-try:
-    from django.conf import settings
-except ImportError:
-    settings = None
 
 
-__version__ = '0.3.0'
-
-
-if settings is not None and settings.configured:
-    from sentry.plugins import plugins, register
-
-    from plugin import TelegramNotificationsPlugin
-
-    if TelegramNotificationsPlugin.slug not in [plugin.slug for plugin in plugins.all()]:
-        register(TelegramNotificationsPlugin)
+__version__ = '0.4.0'
